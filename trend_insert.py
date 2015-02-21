75f1f1291280d3ccb575949bb8beb3bb9bff2410
Abhishek Bhattacharjee <abhishek.bhattacharjee11@gmail.com>
2015-02-21 21:05:20 +0530

    Removed consumer key and consumer secret from code.

--- trend_insert.py
+++ trend_insert.py
@@ -29,6 +29,7 @@ import pprint as pp
 from pymongo import MongoClient
 from bson.objectid import ObjectId as objid
 import pdb
+import contants
 
 def put_into_database(trendname,screename,text,loc=None):
     try:
@@ -56,14 +57,14 @@ def get_info_for_trends(x,count=None):
         conn.request("GET","/1.1/search/tweets.json?q="+str(x['query']),"",get_headers)
     else:
         conn.request("GET","/1.1/search/tweets.json?q="+str(x['query'])+"&count=100","",get_headers)
-    
+
     tweets_resp = conn.getresponse()
     tweets = tweets_resp.read()
     tweets_json = json.loads(str(tweets))
     i=1
     #print pp.pprint(tweets_json)
     for s in tweets_json['statuses']:
-        #printing names of users and tweets for each trending topic 
+        #printing names of users and tweets for each trending topic
         print str(i)+". " +s['user']['name'].encode('utf-8')
         print s['text'].encode('utf-8')
         location = s['user']['location']
@@ -79,11 +80,8 @@ def get_info_for_trends(x,count=None):
             put_into_database(x['name'],s['user']['screen_name'],s['text'])
         i=i+1
 
-CONSUMER_KEY='tI0bUJuctVgzA82wGYLiQ'
-CONSUMER_SECRET='59GVWA6j7RJt1Ntw2cFi57FS91jzRFIk6lbNzH8Cs8'
-
-#OAUTH_TOKEN='165807027-Y9GeHly2EiJ5LBzcI3eAGvNp9M44kpO5tK9yfu2n'
-#OAUTH_TOKEN_SECRET='qTopfcbhvo1qo3RvWvqzxDg2IHeJETyz8x5SPn8n0zk'
+CONSUMER_KEY = contants.CONSUMER_KEY
+CONSUMER_SECRET = contants.CONSUMER_SECRET
 
 enc_str= base64.b64encode(CONSUMER_KEY+":"+CONSUMER_SECRET)
 
@@ -97,7 +95,7 @@ if __name__ == "__main__":
         param = urllib.urlencode({'grant_type':'client_credentials'})
         headers = {"Authorization":"Basic "+enc_str,
                    "Content-type": "application/x-www-form-urlencoded;charset=UTF-8"}
-        
+
 
         conn.request("POST","/oauth2/token/",param,headers)
         response=conn.getresponse()
@@ -108,17 +106,17 @@ if __name__ == "__main__":
         access_token = dic.get("access_token")
 
         get_headers={"Authorization":"Bearer "+access_token}
-        
-        ##Getting WorldWide Trends 
-        conn.request("GET","/1.1/trends/place.json?id=1","",get_headers)  
+
+        ##Getting WorldWide Trends
+        conn.request("GET","/1.1/trends/place.json?id=1","",get_headers)
         get_resp = conn.getresponse()
         sample = get_resp.read()
-        
-        ##converting the received string in JSON form 
+
+        ##converting the received string in JSON form
         data = json.loads(str(sample))
         #print pp.pprint(data[0])
         names = data[0]['trends']
-        
+
         print "-----------------------------Trends-----------------------------------"
         for x in names:
             try:
@@ -128,9 +126,8 @@ if __name__ == "__main__":
                     get_info_for_trends(x)
                 except:
                     print "Error.... :( "
-            
+
     except:
         print "something went wrong :("
-            
-    print "\n"
 
+    print "\n"
